5ea5c6a04f6e55af16a3dc40ea08ef1aa19c6e4c
danielvivacqua <drvivacqua@hotmail.com>
2017-07-05 15:15:24 -0400

    Reviewed concepts

--- LoopsPracticeSummer17/Program.cs
+++ LoopsPracticeSummer17/Program.cs
@@ -10,6 +10,20 @@ namespace LoopsPracticeSummer17
     {
         static void Main(string[] args)
         {
+            //When would you use a loop?
+            //Why would you use a loop?
+            //Nested Loops
+            //Infinite Loops
+            //Keywords break and continue
+
+            //For Loop
+            //Can be used for both counting and ...
+            // ITERATING through an array (or other collection type)
+            //Looks like this:
+            //for(initializer; condition; updater)
+            //{
+            //     Do something
+            //}
         }
     }
 }
